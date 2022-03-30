b766d8dd238fd21762b0c8ab13b1a01a93b93c33
Aditya <sam.aditya297@gmail.com>
2022-03-30 20:49:56 +0530

    exeption handling

--- remove_element/remove_element.py
+++ remove_element/remove_element.py
@@ -5,12 +5,15 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        count = 0
-        for i in range(len(nums)):
-            if nums[i]/val != 1:
-                nums[count] = nums[i]
-                count += 1
-        return count
+        if len(nums) == 0:
+            return 0
+        else:
+            count = 0
+            for i in range(len(nums)):
+                if nums[i] != val:
+                    nums[count] = nums[i]
+                    count += 1
+            return count
 
 
 print(Solution.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
